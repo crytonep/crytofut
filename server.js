ff6c25c4b314b4a0d8a9614561c212f2d9aa4659
crytonep <76454183+crytonep@users.noreply.github.com>
2022-09-20 11:26:32 -0300

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELI
 var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
   originBlacklist: originBlacklist,
-  originWhitelist: ["https://megadeportes.online", "https://gregorsamsa.life", "https://vercomofutbol.xyz", "https://megatelevisionhd.com", "https://f2njrhs8nqn5.blogspot.com"],
+  originWhitelist: ["https://megadeportes.online", "https://gregorsamsa.life", "https://vercomofutbol.xyz", "https://megatelevisionhd.com"],
   requireHeader: ['origin', 'x-requested-with'],
   checkRateLimit: checkRateLimit,
   removeHeaders: [
